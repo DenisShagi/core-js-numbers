4da713a928d6dd63ccacefff74dabc6ad47d5d74
Denis <denis.shagim@icloud.com>
2024-11-16 17:10:33 +0500

    18 passing test

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -229,8 +229,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return +value || def;
 }
 
 /**
@@ -244,8 +244,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -261,8 +261,13 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+
+function getFibonacciNumber(index) {
+  const res = [0, 1];
+  for (let i = 2; i <= index; i += 1) {
+    res.push(res.at(-2) + res.at(-1));
+  }
+  return res[index];
 }
 
 /**
@@ -276,8 +281,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let res = 0;
+  for (let i = 1; i <= n; i += 1) {
+    res += i;
+  }
+  return res;
 }
 
 /**
@@ -291,8 +300,13 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const strOfNum = num.toString().split('');
+  let result = 0;
+  strOfNum.forEach((element) => {
+    result += +element;
+  });
+  return result;
 }
 
 /**
